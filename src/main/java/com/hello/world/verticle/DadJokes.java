0ec89de42bcfaf720271f7f6ddb15158d74385be
Mwangi Gikonyo <timomwa@gmail.com>
2022-11-09 15:53:15 +0300

    Dad Jokes API with RDBMS

---
+++ src/main/java/com/hello/world/verticle/DadJokes.java
@@ -0,0 +1,33 @@
+package com.hello.world.verticle;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface DadJokes {
+	
+	public static List<String> jokes = Arrays.asList(
+			"I'm afraid for the calendar. Its days are numbered.",
+			"My wife said I should do lunges to stay in shape. That would be a big step forward.",
+			"Why do fathers take an extra pair of socks when they go golfing? In case they get a hole in one!",
+			"Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.",
+			"What do a tick and the Eiffel Tower have in common? They're both Paris sites.",
+			"What do you call a fish wearing a bowtie? Sofishticated.",
+			"How do you follow Will Smith in the snow? You follow the fresh prints.",
+			"If April showers bring May flowers, what do May flowers bring? Pilgrims.",
+			"I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along.",
+			"What do you call a factory that makes okay products? A satisfactory.",
+			"Dear Math, grow up and solve your own problems.",
+			"What did the janitor say when he jumped out of the closet? Supplies!",
+			"What did the ocean say to the beach?\" \"Nothing, it just waved.",
+			"Why do seagulls fly over the ocean? Because if they flew over the bay, we'd call them bagels.",
+			"I only know 25 letters of the alphabet. I don't know y.",
+			"How does the moon cut his hair? Eclipse it.",
+			"What did one wall say to the other? I'll meet you at the corner.",
+			"What did the zero say to the eight? That belt looks good on you.",
+			"A skeleton walks into a bar and says, 'Hey, bartender. I'll have one beer and a mop.",
+			"Where do fruits go on vacation? Pear-is!",
+			"I asked my dog what's two minus two. He said nothing.",
+			"What did Baby Corn say to Mama Corn? Where's Pop Corn?",
+			"What's the best thing about Switzerland? I don't know, but the flag is a big plus.");
+
+}
--- src/main/java/com/hello/world/verticle/HelloWorldVerticle.java
+++ src/main/java/com/hello/world/verticle/HelloWorldVerticle.java
@@ -3,9 +3,9 @@ package com.hello.world.verticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.jdbcclient.JDBCPool;
 import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.Tuple;
 
 /**
  * All Verticles extend the abstract 
@@ -19,22 +19,34 @@ import io.vertx.sqlclient.Row;
  */
 public class HelloWorldVerticle extends AbstractVerticle {
 	
-	private final int SERVER_PORT = 8787;
-	private JDBCPool pool;
+	protected final int SERVER_PORT = 8787;
+	protected JDBCPool pool;
+	private final String DAD_JOKE_TABLE = "dad_joke211";
+	private final String SQL_CREATE_DAD_JOKE_TABLE = "CREATE TABLE IF NOT EXISTS "+DAD_JOKE_TABLE+"(id INT NOT NULL AUTO_INCREMENT, joke VARCHAR(350) NOT NULL)";
+	private final String SQL_PREP_STATEMENT_ADD_DAD_JOKE = "INSERT INTO "+DAD_JOKE_TABLE+"(joke) VALUES(?)";
+	private final String SQL_GET_RANDOM_DAD_JOKE = "SELECT joke from "+DAD_JOKE_TABLE+" ORDER BY RANDOM() LIMIT 1";
 
 	@Override
 	public void start(Promise<Void> startPromise) throws Exception {
-		
 		setup();
-		
-		
-		vertx.createHttpServer().requestHandler(
+		vertx.createHttpServer()
+		.requestHandler(
 				req -> {
+					
+					this.pool
+					.query(SQL_GET_RANDOM_DAD_JOKE)
+					.execute()
+					.onFailure(handler->{
+						System.out.println("Error performing SQL -> : "+handler.getMessage());
+					})
+					.onSuccess(rowSet->{
+						Row row = rowSet.iterator().next();
+						String randomDadJoke = row.getString(0);
 						req
 						.response()
 						.putHeader("content-type", "text/plain")
-						.end("Hello World!\r\nThis is from a Verticle!");
-						
+						.end(randomDadJoke);
+					});
 		}).listen(SERVER_PORT, http -> {
 			
 			if (http.succeeded()) {
@@ -43,10 +55,10 @@ public class HelloWorldVerticle extends AbstractVerticle {
 			} else {
 				startPromise.fail(http.cause());
 			}
-			
 		});
 	}
 	
+
 
 	/**
 	 * Sets up the boilerplate code, not
@@ -62,65 +74,59 @@ public class HelloWorldVerticle extends AbstractVerticle {
 		createH2DbPool();
 		//Create the Database
 		createDatabase();
-		//Add Dad Jokes to the database
-		addDadJokes();
+		
 	}
 
+	/**
+	 * Adds dad jokes to the database from 
+	 * com.hello.world.verticle.DadJokes
+	 */
 	private void addDadJokes() {
-		this.pool
-		.query("INSERT INTO dad_jokes4(1, 'Why didn\'t the bike move?\n\rBecause it was two tyred.')")
-		.execute().onFailure(e->{
-			System.out.println("\n\r\t🔥 Failed to add Dad Jokes to Database ");
-		})
-		.onSuccess( rows ->{
-			System.out.println("\n\r\t✅ Dad Joke Added. ");
+		
+		DadJokes
+		.jokes
+		.forEach( joke -> {
+			this.pool
+			.preparedQuery(SQL_PREP_STATEMENT_ADD_DAD_JOKE)
+			.execute(Tuple.of(joke))
+			.onSuccess(rows ->{
+			}).onFailure(fl->{
+			});
 		});
 		
 	}
 
+	/**
+	 * Creates the dad jokes database
+	 */
 	private void createDatabase() {
-		
-		JDBCClient jdbcClient = JDBCClient.createShared(vertx, new JsonObject()
-                .put("url", "jdbc:h2:~/test")
-                .put("user", "sa")
-                .put("password", "")
-                .put("driver_class", "org.h2.Driver")
-                .put("max_pool_size", 16));
-		
-		jdbcClient
-		.query("CREATE TABLE dad_jokes4(id INT NOT NULL, joke VARCHAR(50) NOT NULL);", e->{
-			System.out.println("\n\t\t >>> "+e);
-		});
-		/*
 		this.pool
-		.query("CREATE TABLE dad_jokes3(id INT NOT NULL, joke VARCHAR(50) NOT NULL);")
+		.query(SQL_CREATE_DAD_JOKE_TABLE)
 		.execute()
 		.onFailure(e->{
-			
-			e.printStackTrace();
-			System.out.println("\n\r\t🔥 Failed to create Dad Jokes Database ");
-			
+			System.out.println("\n\r\t🔥 Failed to create Dad Jokes Database Error: "+e.getMessage());
 		}).onSuccess(rows ->{
-			
 			System.out.println("\n\r\t✅ Dad Jokes Database Created. ");
-			System.out.println("\n\r\t✅ Rows:  "+rows.size());
-			for (Row row : rows) {
-			      System.out.println(row.getColumnName(1));
-			 }
-		});*/
-		
+			
+			//Add Dad Jokes to the database
+			addDadJokes();
+		});
 	}
 
+	/**
+	 * Create a database connection pool
+	 * 
+	 */
 	private void createH2DbPool() {
 		final JsonObject config = new JsonObject()
-				  .put("url", "jdbc:h2:~/test")
-				  .put("datasourceName", "pool-name")
+				  .put("url", "jdbc:h2:~/test2")
 				  .put("username", "sa")
 				  .put("password", "")
+				  .put("driver_class", "org.h2.Driver")
 				  .put("initial_pool_size", 3)
-				  .put("max_pool_size", 16);
+				  .put("max_pool_size", 5);
 		this.pool = JDBCPool.pool(vertx, config);
-		System.out.println("\n\r\t✅ H2 Db pool initialized Successfully. Initial Pool size: "+this.pool.size());
-		
+		this.pool.getConnection();
+		System.out.println("\n\r\t✅ H2 Db pool initialized Successfully. ");
 	}
 }
